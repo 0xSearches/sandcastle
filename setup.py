30f97f945edad4ea9388469ed08391d69dca9547
yasinS <yasinsoliman@icloud.com>
2017-04-09 15:14:42 +0100

    Cleanup original setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'sandcastle',
-  packages = ['sandcastle'], # this must be the same as the name above
   version = '1.2.0',
   description = 'A Python script for AWS S3 bucket enumeration.',
   author = 'Yasin Soliman',
   author_email = 'ysx.public@icloud.com',
   scripts=['sandcastle'],
-  url = 'https://github.com/yasinS/sandcastle', # use the URL to the github repo
-  download_url = 'https://github.com/yasinS/sandcastle/archive/1.2.0.tar.gz', # I'll explain this in a second
-  keywords = ['amazons3', 'infosec', 'bucket'], # arbitrary keywords
+  url = 'https://github.com/yasinS/sandcastle',
+  download_url = 'https://github.com/yasinS/sandcastle/archive/1.2.0.tar.gz',
+  keywords = ['amazons3', 'infosec', 'bucket'],
   classifiers = [],
 )
